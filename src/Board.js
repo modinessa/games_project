8abfd1a1a4114ff20b5102e41dbdbb06a3e104c7
Inessa Modina <modina.inessa@gmail.com>
2022-07-19 15:46:06 -0700

    Initial commit

---
+++ src/Board.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Tile from "./Tile";
+import { TILE_COUNT, GRID_SIZE, BOARD_SIZE } from "./constants"
+
+function Board() {
+	const [tiles, setTiles] = useState([...Array(TILE_COUNT).keys()]);
+
+	const pieceWidth = Math.round(BOARD_SIZE / GRID_SIZE);
+	const pieceHeight = Math.round(BOARD_SIZE / GRID_SIZE);
+	const style = {
+		width: BOARD_SIZE,
+		height: BOARD_SIZE,
+	};
+
+	return (
+		<>
+			<ul style={style} className="board">
+
+			</ul>
+		</>
+	)
+}
