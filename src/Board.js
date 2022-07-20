dbecbd57dce20eb2054f2cc7f5ac708f2ae4ab1a
Inessa Modina <modina.inessa@gmail.com>
2022-07-19 17:21:09 -0700

    Add helpers functions

--- src/Board.js
+++ src/Board.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Tile from "./Tile";
 import { TILE_COUNT, GRID_SIZE, BOARD_SIZE } from "./constants"
 
-function Board() {
+export default function Board() {
 	const [tiles, setTiles] = useState([...Array(TILE_COUNT).keys()]);
 
 	const pieceWidth = Math.round(BOARD_SIZE / GRID_SIZE);
@@ -15,8 +15,17 @@ function Board() {
 	return (
 		<>
 			<ul style={style} className="board">
-
+				{tiles.map((tile, index) => (
+					<Tile
+						key={tile}
+						index={index}
+						tile={tile}
+						width={pieceWidth}
+						height={pieceHeight}
+					/>
+				))}
 			</ul>
+			<button> Start game</button>
 		</>
 	)
 }
